f135da7bfd7e0d3a39295c0c6a5aff1ecebeb2aa
calebgeb12 <calebgeb55@gmail.com>
2025-04-28 22:14:08 -0400

    small issue

--- testing/Testing.java
+++ testing/Testing.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Testing {
-    public static void main(String[] args) {;
+    public static void main(String[] args) {
 
     //data sets used for testing
         double[][] BURST_SETS = {
